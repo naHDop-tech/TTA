7b1f78e4bf00a05c27a954f57bac2b30134d05ec
Grigory <tarasov198726@gmail.com>
2022-04-15 21:04:38 +0300

    fix: minor fixing

--- server/src/abstractions/city.abstraction.ts
+++ server/src/abstractions/city.abstraction.ts
@@ -2,17 +2,14 @@ import { Expose } from 'class-transformer';
 
 import { Weathers } from '@constants/city.constant'
 import { WeatherGenerator } from '@root/utils/WeatherGenerator'
-
+import { BranchOffice } from '@abstractions/branch-office.abstract'
 // const getPopulation = Math.floor(Math.random() * (7000000 - 5000) + 5000)
 
 export class City {
     readonly id: number
-
     name: string
     population: number
-    // TODO: office
-    branchOffices: any[]
-
+    branchOffices: BranchOffice
 
     @Expose()
     get weather(): Weathers {
